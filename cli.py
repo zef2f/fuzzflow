9d3779d34f058d57c0642fd4115b4cfcd545f027
Denis Sergeev <zeff@altlinux.org>
2025-03-10 18:10:57 +0300

    feat: add fuzzflow cli-interface

---
+++ cli.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+import logging
+from src.utils import parse_cli_args
+from src.orchestrator import Orchestrator
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+def main():
+    """
+    Точка входа в fuzzflow.
+    Считывает параметры CLI и запускает Orchestrator.
+    """
+
+    cli_args = parse_cli_args()
+    logging.debug(f"Аргументы получены: wrapper_names={cli_args.wrapper_names}, "
+                  f"memory_limit={cli_args.memory_limit}, "
+                  f"single_fuzz_script={cli_args.single_fuzz_script}")
+
+    try:
+        orchestrator = Orchestrator(
+            wrapper_names=cli_args.wrapper_names,
+            memory_limit=cli_args.memory_limit,
+            single_fuzz_script=cli_args.single_fuzz_script
+        )
+
+        orchestrator.run()
+
+    except Exception as e:
+        logging.error(f"Ошибка в работе fuzzflow: {e}", exc_info=True)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
